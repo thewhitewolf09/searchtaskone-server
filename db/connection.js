486d9d4c7e7375b44b04b68a316467a7504abd33
thewhitewolf09 <sumitramprakashnirmal@gmail.com>
2022-08-20 17:53:41 +0530

    first commit

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose')
-//DATABASE =  mongodb+srv://nrmlsumit:Sumit210%40@cluster0.hahwy.mongodb.net/gograbmoney?retryWrites=true&w=majority
-//DATABASE =  mongodb://localhost:27017
 const DB = process.env.DATABASE
 
 mongoose.connect(DB).then(() => {
