ee2d763474c3ce8704405195c73dca8ceb0064b6
thewhitewolf09 <sumitramprakashnirmal@gmail.com>
2022-08-20 18:04:50 +0530

    first commit

--- index.js
+++ index.js
@@ -4,17 +4,7 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-const whitelist = ["https://6300ceec5f3879453b891d96--legendary-babka-b9d515.netlify.app/","http://localhost:3000"];
-const corsOptions = {  
-    credentials: true, // This is important.
-    origin: (origin, callback) => {
-        if (whitelist.includes(origin))
-            return callback(null, true)
-
-        callback(new Error('Not allowed by CORS'));
-    }
-}
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 dotenv.config({ path: './config.env' })
 
